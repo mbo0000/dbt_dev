4f28d308a3f99f5eab691770abbc302a26738b90
Minh Bo <mbo0000DA@gmail.com>
2024-11-13 14:46:20 -0800

    adding team conference (#9)
    * add year field to team stat
    
    * adding team conference
    
    ---------
    
    Co-authored-by: Minh <minh.bo@checkr.com>

---
+++ macros/set_team_conference.sql
@@ -0,0 +1,13 @@
+
+
+{% macro set_team_conference(team) -%}
+    
+    {% set east_team = ('Atlanta Hawks', 'Boston Celtics', 'Brooklyn Nets', 'Charlotte Hornets', 'Chicago Bulls', 'Cleveland Cavaliers', 'Detroit Pistons', 'Indiana Pacers', 'Miami Heat', 'Milwaukee Bucks', 'New York Knicks', 'Orlando Magic', 'Philadelphia 76ers', 'Toronto Raptors', 'Washington Wizards') %}
+    {% set west_team = ('Dallas Mavericks', 'Denver Nuggets', 'Golden State Warriors', 'Houston Rockets', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Memphis Grizzlies', 'Minnesota Timberwolves', 'New Orleans Pelicans', 'Oklahoma City Thunder', 'Phoenix Suns', 'Portland Trail Blazers', 'Sacramento Kings', 'San Antonio Spurs', 'Utah Jazz') %}
+
+    case 
+        when {{team}} in {{east_team}} then 'East'
+        when {{team}} in {{west_team}} then 'West' 
+    end
+
+{%- endmacro %}
