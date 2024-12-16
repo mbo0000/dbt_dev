7f4e14597a248a36a3c858caeeff5c30db5a19f6
Minh Bo <mbo0000DA@gmail.com>
2024-12-15 18:16:55 -0800

    Dev (#34)
    * add year field to team stat
    
    * adding team conference
    
    * add year to player stat
    
    * fix year ref field
    
    * convert data type
    
    * player cummulative stat per season
    
    * moving player cum stat to analytics
    
    * remove from clean
    
    * revise player cum stat
    
    * fix field red
    
    * syntax error
    
    * revise model config
    
    * team stat extended view
    
    * resolved merge conflix
    
    * average per game
    
    * adding team advanced stats model
    
    * fix columns name
    
    * adding off and def ratings
    
    * update profile and creds config
    
    * player game stat pct fix
    
    * syntax error
    
    * use games date in player game stat
    
    * field ref
    
    ---------
    
    Co-authored-by: Minh <minh.bo@checkr.com>

--- models/source/nba/player_game_stat.sql
+++ models/source/nba/player_game_stat.sql
@@ -3,34 +3,34 @@
 ) }}
 
 select 
-    AST
-    ,BLK
-    ,DREB
-    ,FG3A
-    ,FG3M
-    ,div0(FG3M,FG3A) as FG3_PCT
-    ,FGA
-    ,FGM
-    ,div0(FGM,FGA) as FG_PCT
-    ,FTA
-    ,FTM
-    ,div0(FTM,FTA) as FT_PCT
+    p.AST
+    ,p.BLK
+    ,p.DREB
+    ,p.FG3A
+    ,p.FG3M
+    ,div0(p.FG3M,p.FG3A) as FG3_PCT
+    ,p.FGA
+    ,p.FGM
+    ,div0(p.FGM,p.FGA) as FG_PCT
+    ,p.FTA
+    ,p.FTM
+    ,div0(p.FTM,p.FTA) as FT_PCT
     ,g.GAME_DATE
-    ,GAME_ID
-    ,ID
-    ,MATCHUP
-    ,MIN
-    ,OREB
-    ,PF
-    ,PLAYER_ID
-    ,PLUS_MINUS
-    ,PTS
-    ,REB
-    ,SEASON_ID
-    ,STL
-    ,TOV
-    ,VIDEO_AVAILABLE
-    ,WL
-    , try_to_number(right(SEASON_ID, 4)) as year
+    ,p.GAME_ID
+    ,p.ID
+    ,p.MATCHUP
+    ,p.MIN
+    ,p.OREB
+    ,p.PF
+    ,p.PLAYER_ID
+    ,p.PLUS_MINUS
+    ,p.PTS
+    ,p.REB
+    ,p.SEASON_ID
+    ,p.STL
+    ,p.TOV
+    ,p.VIDEO_AVAILABLE
+    ,p.WL
+    , try_to_number(right(p.SEASON_ID, 4)) as year
 from {{source('raw', 'player_game_stat')}} as p
     left join {{ref('games')}} as g on p.game_id = p.game_id
